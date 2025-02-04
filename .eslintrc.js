d8ef15dbccf8be3eba8d2026e031a28bcc474f43
TwoMochi <gohastr2000@gmail.com>
2025-02-04 19:40:31 +0300

    add checks 19:40

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
+        'eslint space-before-blocks': ["never"],
         'i18next/no-literal-string': [
             'error',
             {
