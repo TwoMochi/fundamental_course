9e85298db820c899ba26885ff4f673a6e95818ab
TwoMochi <gohastr2000@gmail.com>
2025-02-11 10:45:01 +0300

    master changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,25 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useState } from 'react';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Modal } from 'shared/ui/Modal/Modal';
 
-function App() {
+
+ App() {
     const { theme } = useTheme();
 
-    return (
+    const [isOpen, setIsOpen] = useState(false);eturn (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
-                <div className="content-page">
+      
+                <button onClick={() => setIsOpen(true)}>toggle</button>
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis vero maxime, dolores quaerat cupiditate ratione eius veritatis explicabo sit, nostrum deserunt, hic blanditiis sequi distinctio quisquam aut odit architecto asperiores.
+                </Modal>          <div className="content-page">
                     <Sidebar />
                     <AppRouter />
                 </div>
