f80c14fc45d6cd8aa194f8f07131f2d0c0ded6ec
TwoMochi <gohastr2000@gmail.com>
2025-02-24 17:20:24 +0300

    fix strict ts mode and fix unit tests

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,7 +1,9 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { profileReducer } from 'entities/Profile';
+import { fetchProfileData, ProfileCard, profileReducer } from 'entities/Profile';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useEffect } from 'react';
 
 const reducers: ReducersList = {
     profile: profileReducer,
@@ -16,12 +18,18 @@ const ProfilePage = (props: ProfilePageProps) => {
         className,
     } = props;
 
+    const dispatch = useAppDispatch();
+
     const { t } = useTranslation();
+
+    useEffect(() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div className={classNames('', {}, [className])}>
-                {t('PROFILE PAGE')}
+                <ProfileCard />
             </div>
         </DynamicModuleLoader>
 
